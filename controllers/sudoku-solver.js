a65eecd63634d0b1b11d1daaf3aeee379bb180d5
PromiseFru <promisefru1@gmail.com>
2020-11-14 20:40:13 +0100

    validator function updated

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,6 +1,10 @@
 class SudokuSolver {
 
   validate(puzzleString) {
+    var check = puzzleString.match(/[^1-9.]/g);
+
+    if(check) return {error: 'Invalid characters in puzzle'};
+    return puzzleString;
   }
 
   checkRowPlacement(puzzleString, row, column, value) {
