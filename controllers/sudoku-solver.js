36e2b9962fbfca57e238a84f56669a492f862792
Promise Fru <promisefru1@gmail.com>
2020-11-24 11:39:08 +0100

    updates made

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -26,7 +26,7 @@ class SudokuSolver {
       board.push(boardRow)
     }
 
-    console.log(board);
+    // console.log(board);
     return board;
   }
 
@@ -50,13 +50,13 @@ class SudokuSolver {
     var i;
     for (i = 0; i < 9; i++) {
       if (board[row][i] == value) {
-        // console.log(false);
+        // console.log("row check false");
         return {
           valid: false
         };
       }
     }
-    // console.log(true);
+    // console.log("row check true");
     return {
       valid: true
     }
@@ -68,59 +68,92 @@ class SudokuSolver {
 
     for (i = 0; i < 9; i++) {
       if (board[i][column] == value) {
-        // console.log(false)
+        // console.log("col check false")
         return {
           valid: false
         }
       }
     }
-    // console.log(true)
-    return true;
+    // console.log("col check true")
+    return {
+      valid: true
+    };
   }
 
   checkRegionPlacement(puzzleString, row, column, value) {
     var board = this.boardParser(puzzleString);
     var i, j;
     var regRow = Math.floor(row / 3) * 3;
-    var regCol = Math.floor(col / 3) * 3;
+    var regCol = Math.floor(column / 3) * 3;
 
     for (i = 0; i < 3; i++) {
       for (j = 0; j < 3; j++) {
         if (board[regRow + i][regCol + j] == value) {
-          // console.log(false)
+          // console.log("reg check false")
           return {
             valid: false
           }
         }
       }
     }
-    // console.log(true);
+    // console.log("reg check true");
     return {
       valid: true
     }
   }
 
   checkvalue(puzzleString, row, column, value) {
-    if (this.checkRowPlacement(puzzleString, row, column, value).valid && this.checkColPlacement(puzzleString, row, column, value).valid && this.checkRegionPlacement(puzzleString, row, column, value).valid) {
+    var rowCheck = this.checkRowPlacement(puzzleString, row, column, value).valid
+    var colCheck = this.checkColPlacement(puzzleString, row, column, value).valid
+    var regCheck = this.checkRegionPlacement(puzzleString, row, column, value).valid
+    // console.log(rowCheck, colCheck, regCheck);
+    
+    if (rowCheck && colCheck && regCheck) {
+      // console.log(true)
       return true;
     }
+    // console.log(false)
     return false;
   }
 
   solve(puzzleString) {
+    var board = this.boardParser(puzzleString)
+    var emptySpot = this.getDot(puzzleString);
+    var row = emptySpot[0];
+    var column = emptySpot[1];
+    var i;
 
+    if (row == -1) {
+      return board
+    }
+
+    for (i = 1; i <= 9; i++) {
+      if (this.checkvalue(puzzleString, row, column, i)) {
+        board[row][column] = i;
+        this.solve(puzzleString);
+      }
+    }
+
+    // if (row != -1) {
+    //   board[row][column] = ".";
+    // }
+
+    console.log(board);
+    return board;
   }
 }
 
-const input = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
-const row = 0;
-const col = 1;
-const value = 7;
+const input = '..839.7.575.....964..1.......16.29846.9.312.7..754.....62..5.78.8...3.2...492...1';
+const row = 1;
+const col = 0;
+const value = 3;
 
-new SudokuSolver().checkRowPlacement(input, row, col, value);
-new SudokuSolver().checkColPlacement(input, row, col, value)
-new SudokuSolver().checkRegionPlacement(input, row, col, value)
-new SudokuSolver().boardParser(input);
+// new SudokuSolver().checkRowPlacement(input, row, col, value);
+// new SudokuSolver().checkColPlacement(input, row, col, value)
+// new SudokuSolver().checkRegionPlacement(input, row, col, value)
+// new SudokuSolver().boardParser(input);
+new SudokuSolver().solve(input);
+// new SudokuSolver().checkvalue(input, row, col, value)
 
 
 module.exports = SudokuSolver;
