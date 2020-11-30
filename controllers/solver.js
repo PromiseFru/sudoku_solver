94ac0a5a2873524f4713122f09f925dfbc36c103
Promise Fru <promisefru1@gmail.com>
2020-11-30 05:53:20 +0100

    searchForSolution function created

--- controllers/solver.js
+++ controllers/solver.js
@@ -1,9 +1,25 @@
-function solve(bosard) {
+function solve(boards) {
     // check if board is solved
     if (solved(board)) {
         return board
     } else {
         const posibilities = nextBoards(boards);
-        const validBoard = keepOnlyValid(posibilities);
+        const validBoards = keepOnlyValid(posibilities);
+        return searchForSolution(validBoards);
+    }
+}
+
+function searchForSolution(boards) {
+    if (boards < 1) {
+        return false
+    } else {
+        // Backtraking search for solution
+        var firstBoard = boards.shift();
+        const trypath = solve(firstBoard);
+        if (trypath != false) {
+            return trypath
+        } else {
+            return searchForSolution(boards)
+        }
     }
 }
