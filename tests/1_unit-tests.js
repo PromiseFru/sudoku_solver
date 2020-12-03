2dfceb2402ef82d8293e439f3f07e02a249d6818
Promise Fru <promisefru1@gmail.com>
2020-12-03 16:12:50 +0100

    unit test updated

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -142,22 +142,29 @@ suite('UnitTests', () => {
     // Valid complete puzzles pass
     test('Valid puzzles pass', done => {
       const input = '769235418851496372432178956174569283395842761628713549283657194516924837947381625';
+      var result = solver.solve(input);
 
-      // done();
+      assert.equal(result, input)
+      done();
     });
 
     // Invalid complete puzzles fail
     test('Invalid puzzles fail', done => {
       const input = '779235418851496372432178956174569283395842761628713549283657194516924837947381625';
+      var result = solver.solve(input);
 
-      // done();
+      assert.equal(result, 'Invalid Solution')
+      done();
     });
 
     // Returns the expected solution for a valid, incomplete puzzle
     test('Returns the expected solution for an incomplete puzzle', done => {
       const input = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
+      const solution = '769235418851496372432178956174569283395842761628713549283657194516924837947381625';
+      var result = solver.solve(input);
 
-      // done();
+      assert.equal(result, solution)
+      done();
     });
   });
 });
