26b052504be283d673fab3433b0c88df186273f5
Promise Fru <promisefru1@gmail.com>
2020-12-05 10:10:58 +0100

    invalid xter in string test pass

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -47,11 +47,21 @@ suite('Functional Tests', () => {
     });
 
     test('Invalid Characters in Puzzle', done => {
+      const input = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..o..';
       const error = {
         error: 'Invalid characters in puzzle'
       };
 
-      // done();
+      chai.request(server)
+        .post('/api/solve')
+        .send({
+          puzzle: input
+        })
+        .end((req, res) => {
+          assert.equal(res.body.error, error.error);
+        })
+
+      done();
     });
 
     test('Puzzle incorrect length', done => {
