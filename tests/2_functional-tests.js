3568ad57ae6103b7d736466f67365e0c669afda3
Promise Fru <promisefru1@gmail.com>
2020-12-05 10:16:04 +0100

    functional test upodated

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -65,11 +65,21 @@ suite('Functional Tests', () => {
     });
 
     test('Puzzle incorrect length', done => {
+      const input = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..66..';
       const error = {
         error: 'Expected puzzle to be 81 characters long'
       };
 
-      // done();
+      chai.request(server)
+        .post('/api/solve')
+        .send({
+          puzzle: input
+        })
+        .end((req, res) => {
+          assert.equal(res.body.error, error.error);
+        })
+
+      done();
     });
 
     test('Puzzle Cannot be Solved', done => {
@@ -78,6 +88,16 @@ suite('Functional Tests', () => {
         error: 'Puzzle cannot be solved'
       };
 
+      chai.request(server)
+        .post('/api/solve')
+        .send({
+          puzzle: input
+        })
+        .end((req, res) => {
+          assert.equal(res.body.error, error.error);
+        })
+
+      done();
     });
   });
 
